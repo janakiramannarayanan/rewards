737cee56b8f9fdda307281c7ac3af133a427195a
janakiramannarayanan <45614541+janakiramannarayanan@users.noreply.github.com>
2019-09-13 14:50:20 -0500

    Create ErrorResponseDTO.java

---
+++ src/main/java/com/group/rewards/code/sample/dto/ErrorResponseDTO.java
@@ -0,0 +1,26 @@
+package com.group.rewards.code.sample.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponseDTO extends ResponseDTO {
+
+	@JsonProperty("errorMessage")
+	private String errorMessage;
+
+	public ErrorResponseDTO(String status, String errorMessage) {
+		super(status);
+		this.errorMessage = errorMessage;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+}
