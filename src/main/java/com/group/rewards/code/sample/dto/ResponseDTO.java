7b7b5c5ce73262d5fae52cb51f7309b3e6085a24
janakiramannarayanan <45614541+janakiramannarayanan@users.noreply.github.com>
2019-09-13 14:50:37 -0500

    Create ResponseDTO.java

---
+++ src/main/java/com/group/rewards/code/sample/dto/ResponseDTO.java
@@ -0,0 +1,26 @@
+package com.group.rewards.code.sample.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseDTO {
+
+	@JsonProperty("status")
+	private String status;
+
+	public ResponseDTO(String status) {
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
